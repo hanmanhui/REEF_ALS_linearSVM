d09a0c85ae676e86fd0acc7e4ae2bf3fbc9e8632
hanmanhui <hanmanhui@gmail.com>
2014-10-18 15:26:40 +0900

    Added ObjectToByteArray Class

---
+++ src/main/java/edu/snu/reef/ALS_SVM/ObjectToByteArray.java
@@ -0,0 +1,26 @@
+package edu.snu.reef.ALS_SVM;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import javax.inject.Inject;
+
+public class ObjectToByteArray {
+	@Inject
+	public static final byte[] serialize(Object obj) throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		ObjectOutputStream os = new ObjectOutputStream(out);
+		os.writeObject(obj);
+		return out.toByteArray();
+	}
+	
+	@Inject
+	public static final Object deserialize(byte[] data) throws IOException, ClassNotFoundException {
+		ByteArrayInputStream in = new ByteArrayInputStream(data);
+		ObjectInputStream is = new ObjectInputStream(in);
+		return is.readObject();
+	}
+}
